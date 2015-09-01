1140a40cc0158076ecf9a0b09427ebbdbad7ccfd
Max <hardbitec0c0c0@gmail.com>
2015-09-01 17:59:09 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridgeTasks::Application.config.secret_token = 'abdfe215abb89f44a43c2424efda4f25dc7e074ade359763d6bf0076e77d97edc50fdb313d911bce02a6c8fca7b9dc31b2211c6fbe0e9f83cfee396bbb398cb1'
